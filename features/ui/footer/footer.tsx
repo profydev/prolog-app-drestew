6d4720a0104f463ff78fd2e20832b516af305ce4
drestew <drestew48@gmail.com>
2022-11-20 20:19:27 -0500

    Add footer styling

--- features/ui/footer/footer.tsx
+++ features/ui/footer/footer.tsx
@@ -1,11 +1,51 @@
 import styled from "styled-components";
+import { color } from "@styles/theme";
 
-const FooterEl = styled.div`
+const FooterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  //padding: 0 2rem;
   width: 100%;
-  height: 5rem;
-  background-color: red;
+  background-color: ${color("gray", 50)};
+`;
+
+const FooterContent = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  padding: 0 2rem;
+`;
+const Nav = styled.nav`
+  display: flex;
+  gap: 2rem;
+
+  & > a {
+    text-decoration: none;
+    color: gray;
+    font-weight: 500;
+    margin: 1rem;
+  }
+`;
+
+const Version = styled.p`
+  color: ${color("gray", 400)};
 `;
 
 export function Footer() {
-  return <FooterEl>Footer</FooterEl>;
+  return (
+    <FooterContainer>
+      <FooterContent>
+        <Version>17.05.04</Version>
+        <Nav>
+          <a href="#">Docs</a>
+          <a href="#">API</a>
+          <a href="#">Help</a>
+          <a href="#">Community</a>
+        </Nav>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="/icons/logo-small.svg" alt="Prolog logo" />
+      </FooterContent>
+    </FooterContainer>
+  );
 }
