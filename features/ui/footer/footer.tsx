0a267e16fa1a3d09d95c41166df74e6d67ab875d
drestew <drestew48@gmail.com>
2022-11-20 19:36:03 -0500

    Add footer component

---
+++ features/ui/footer/footer.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const FooterEl = styled.div`
+  width: 100%;
+  height: 5rem;
+  background-color: red;
+`;
+
+export function Footer() {
+  return <FooterEl>Footer</FooterEl>;
+}
