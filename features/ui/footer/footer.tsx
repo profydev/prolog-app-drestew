dc33da25b25c71557324ee47f288bb40780e8b5d
drestew <drestew48@gmail.com>
2022-11-20 20:38:19 -0500

    Add mobile layout

--- features/ui/footer/footer.tsx
+++ features/ui/footer/footer.tsx
@@ -1,11 +1,10 @@
 import styled from "styled-components";
-import { color } from "@styles/theme";
+import { color, breakpoint } from "@styles/theme";
 
 const FooterContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  //padding: 0 2rem;
   width: 100%;
   background-color: ${color("gray", 50)};
 `;
@@ -14,7 +13,29 @@ const FooterContent = styled.div`
   display: flex;
   width: 100%;
   justify-content: space-between;
-  padding: 0 2rem;
+  align-items: center;
+  padding: 1rem;
+
+  @media (max-width: ${breakpoint("desktop")}) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    padding: 2rem;
+    gap: 2rem;
+
+    & > nav {
+      display: flex;
+      gap: 0;
+      order: -2;
+    }
+
+    & > img {
+      order: -1;
+    }
+  }
 `;
 const Nav = styled.nav`
   display: flex;
@@ -24,12 +45,13 @@ const Nav = styled.nav`
     text-decoration: none;
     color: gray;
     font-weight: 500;
-    margin: 1rem;
+    margin: 0 1rem;
   }
 `;
 
 const Version = styled.p`
   color: ${color("gray", 400)};
+  margin: 0;
 `;
 
 export function Footer() {
