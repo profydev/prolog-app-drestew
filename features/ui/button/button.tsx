43d3575d4f11edfde047893d85011d2825c3a1b9
drestew <drestew48@gmail.com>
2022-12-02 21:17:00 -0500

    Add button attributes for story

--- features/ui/button/button.tsx
+++ features/ui/button/button.tsx
@@ -1,6 +1,34 @@
+import React from "react";
 import styled from "styled-components";
 
-export const Button = styled.button`
+export type ButtonSize = "small" | "medium" | "large" | "xlarge";
+export type ButtonColor =
+  | "primary"
+  | "secondary"
+  | "gray"
+  | "empty"
+  | "empty gray"
+  | "error";
+export type ButtonState = "default" | "hover" | "focused" | "disabled";
+export type ButtonIcon = "leading" | "trailing" | "only";
+
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick: () => void;
+  size?: ButtonSize;
+  color?: ButtonColor;
+  state?: ButtonState;
+  icon?: ButtonIcon;
+};
+
+const StyledButton = styled.button<{
+  color?: ButtonColor;
+  size?: ButtonSize;
+  state?: ButtonState;
+  icon?: ButtonIcon;
+  onClick: () => void;
+  children: React.ReactNode;
+}>`
   cursor: pointer;
 
   // remove default button styles
@@ -18,3 +46,24 @@ export const Button = styled.button`
     padding: 0;
   }
 `;
+
+export function Button({
+  color,
+  size,
+  state,
+  icon,
+  onClick,
+  children,
+}: ButtonProps) {
+  return (
+    <StyledButton
+      color={color}
+      size={size}
+      state={state}
+      icon={icon}
+      onClick={onClick}
+    >
+      {children}
+    </StyledButton>
+  );
+}
